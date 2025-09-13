ea2e04e26f5afa62177f19d3bef51dbec2a509a4
HDNIT3 <nguyenhuynh.663459@gmail.com>
2025-09-13 13:52:23 +0700

    tao project

---
+++ src/main/java/vn/iostar/DemoSpringBootBt061Application.java
@@ -0,0 +1,13 @@
+package vn.iostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootBt061Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootBt061Application.class, args);
+	}
+
+}
---
+++ src/test/java/vn/iostar/DemoSpringBootBt061ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.iostar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootBt061ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
